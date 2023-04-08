2b277790c1177c880d1e29dfc56da805a0739d3c
SmokeEtern1tygamingSCPRP <127035504+SmokeEtern1tygamingSCPRP@users.noreply.github.com>
2023-04-08 17:02:34 +0300

    Delete Lesson_2 directory

--- Lesson_2/mainer.py
+++
@@ -1,8 +0,0 @@
-from Lesson_2.student import Student
-from colorama import Fore
-
-
-
-player1 = Student(name='Vasya', birthday='', group=108, GPA=10,
-                    color=Fore.LIGHTRED_EX)
-print(player1)
